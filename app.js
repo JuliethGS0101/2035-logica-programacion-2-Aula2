8ff18fe6975db2cabc144c706a29d0758faf1762
JuliethGS0101 <156958433+JuliethGS0101@users.noreply.github.com>
2024-02-15 16:40:38 -0500

    aula 3

--- app.js
+++ app.js
@@ -1,28 +1,62 @@
-let numeroSecreto = generarNumeroSecreto();
-
+let numeroSecreto = 0;
+let intentos = 1B;
 console.log(numeroSecreto);
 
-function asignarTexto(elemento, texto){
+function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML= texto;
-    return;
-
-}
-function verificarUsuario(){
-    let numeroUsuario = parseInt(document.getElementById('valorUsuario').Value);
-    console.log(typeof(numeroUsuario));
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto));
-    console.log(numeroUsuario);
-    console.log(numeroUsuario === numeroSecreto);
+    elementoHTML.innerHTML = texto;
     return;
 }
 
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } 
+    else {
+        //El usuario no acerto.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El numero secreto es menor');
+        }
+        else{
+            asignarTextoElemento('p', 'El numero secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
 
 function generarNumeroSecreto() {
-     return Math.floor(Math.random()*10)+1;
+    return Math.floor(Math.random()*10)+1;
 }
 
-asignarTexto('h1', 'Juego del numero secreto');
-asignarTexto('p', 'Indica un numero del 1 al 10');
- 
+function condicionesIniciales() {
+   asignarTextoElemento('h1','Juego del número secreto!');
+   asignarTextoElemento('p',`Indica un número del 1 al 10`); 
+   numeroSecreto = generarNumeroSecreto();
+   intentos = 1;
+}
+
+function reiniciarJuego() {
+    // limpiar caja
+    limpiarCaja();
+    // indicar mensaje de intervalo de numero
+    // generar el numero aleatorio
+    // inicializar el nuemro de intentos
+    condicionesIniciales();
+    // deshabilitar el boton de nuevo juego
+    
+
+
+}
+
+condicionesIniciales();
+
